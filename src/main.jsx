f6b920321b577f4e91b75a3d86f14da865f21d2d
Rostyslav-Coder <rostyslavputnikov@gmail.com>
2024-03-14 16:42:23 +0200

    project updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-// ==========  COMPONENT MODULE ========== //
+// ========== MAIN COMPONENT MODULE ========== //
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -8,7 +8,7 @@ import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<Router basename='/'>
+		<Router basename='/restaurant-page'>
 			<App />
 		</Router>
 	</React.StrictMode>,

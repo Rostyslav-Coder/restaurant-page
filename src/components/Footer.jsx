abf016cdedde59bcc4dadc42df208bc53d77e5e1
Rostyslav-Coder <rostyslavputnikov@gmail.com>
2024-03-14 21:44:39 +0200

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,25 @@
 // ========== FOOTER COMPONENT MODULE ========== //
 
+import '../styles/Footer.css';
+
+
 const Footer = () => {
-	return <div><h1>Hi I`m FOOTER</h1></div>
+	return (
+		<div className='footer'>
+			<p className='footer__text'>
+				This website was developed by Rostyslav-Coder using the latest achievements in
+				space science and artifacts of alien visitors.
+			</p>
+			<a
+				className='footer__link'
+				href="https://www.linkedin.com/in/rostyslav-putnikov-76484926a/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				&copy; 2024 Rostyslav-Coder. All rights reserved.
+			</a>
+		</div>
+	);
 };
 
 export default Footer;

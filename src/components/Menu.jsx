224cd76a656f7fb86cac07578060b8846803b528
Rostyslav-Coder <rostyslavputnikov@gmail.com>
2024-03-18 22:31:09 +0200

    project updated

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,6 @@
 // ========== MENU COMPONENT MODULE ========== //
 
+import { ParallaxBanner, Parallax } from 'react-scroll-parallax';
 import MenuCard from './MenuCard';
 import MENU_DATA from '../data/menuData';
 import '../styles/Menu.css';
@@ -8,21 +9,29 @@ import '../styles/Menu.css';
 const Menu = () => {
 	return (
 		<section className='menuPage'>
-			<h1 className='menuPage__title'>Menu</h1>
-			<div className='menuPage__cardsGrp'>
-				{
-					MENU_DATA.map((data) => {
-						return (
-							< MenuCard
-								key={data.cardId}
-								cardTitle={data.cardTitle}
-								cardImage={data.cardImage}
-								cardDescription={data.cardDescription}
-							/>
-						);
-					})
-				}
-			</div>
+			<ParallaxBanner className='menuPage__banner'>
+				<h1 className='menuPage__title'>Menu</h1>
+				<div className='menuPage__cardsGrp'>
+					{
+						MENU_DATA.map((data, index) => {
+							return (
+								<Parallax
+									key={index}
+									speed={30}
+									translateX={[30, -30]}
+								>
+									< MenuCard
+										key={data.cardId}
+										cardTitle={data.cardTitle}
+										cardImage={data.cardImage}
+										cardDescription={data.cardDescription}
+									/>
+								</Parallax>
+							);
+						})
+					}
+				</div>
+			</ParallaxBanner>
 		</section>
 	);
 };
